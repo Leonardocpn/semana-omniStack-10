eb8795a7ec383423395ddc09c627aa0dcc3f6456
Leonardocpn <leonardocpn@gmail.com>
2020-04-03 18:47:39 -0300

    Refatoracao do App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,46 +1,8 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
 import CreateDevForm from "./components/CreateDevForm";
-import { Paper } from "@material-ui/core";
 import ListDev from "./components/DevItem/ListDev";
 import { api } from "./services/api";
-
-export const AppWrapper = styled.div`
-  background-color: #e5e6f0;
-  height: 100vh;
-  -webkit-font-smootihing: antialiased;
-  /* max-width: 1200px; */
-  padding: 60px 30px;
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-  align-items: flex-start;
-  gap: 20px;
-  @media (max-width: 780px) {
-    display: grid;
-    grid-template-columns: 1fr;
-  }
-`;
-
-export const AsideStyled = styled(Paper)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  padding: 20px;
-`;
-
-export const MainStyled = styled.div`
-  display: grid;
-  grid-template-columns: auto auto auto;
-  gap: 20px;
-
-  @media (max-width: 1150px) {
-    grid-template-columns: auto auto;
-  }
-  @media (max-width: 550px) {
-    grid-template-columns: auto;
-  }
-`;
+import { AppWrapper, AsideStyled, MainStyled } from "./styles";
 
 function App() {
   const [devs, setDevs] = useState([]);
@@ -63,7 +25,7 @@ function App() {
         <CreateDevForm onSubmit={handleAddDev} />
       </AsideStyled>
       <MainStyled>
-        {devs.map(dev => (
+        {devs.map((dev) => (
           <ListDev
             key={dev.name}
             name={dev.name}
--- web/src/styles.js
+++ web/src/styles.js
@@ -1,5 +1,39 @@
 import styled from "styled-components";
+import { Paper } from "@material-ui/core";
 
 export const AppWrapper = styled.div`
-  background-color: E5E6F0;
+  background-color: #e5e6f0;
+  height: 100vh;
+  -webkit-font-smootihing: antialiased;
+  /* max-width: 1200px; */
+  padding: 60px 30px;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  align-items: flex-start;
+  gap: 20px;
+  @media (max-width: 780px) {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const AsideStyled = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+`;
+
+export const MainStyled = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  gap: 20px;
+
+  @media (max-width: 1150px) {
+    grid-template-columns: auto auto;
+  }
+  @media (max-width: 550px) {
+    grid-template-columns: auto;
+  }
 `;
